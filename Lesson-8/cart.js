68b15c5884e45f61ebd437def86c33ed42cb78cb
Albacrew <albina-811@mail.ru>
2023-01-27 23:56:01 +0200

    добавлены описания функций

--- Lesson-8/cart.js
+++ Lesson-8/cart.js
@@ -27,6 +27,12 @@ document.querySelector('.product-item-box')
     addProductToCart(id, name, price);
 });
 
+/**
+ * Функция добавления продукта в корзину.
+ * @param {number} id - Id продукта.
+ * @param {string} name - название продукта.
+ * @param {number} price - цена продукта.
+ */
 function addProductToCart(id, name, price) {
     if (!(id in cart)) {
         cart[id] = {
@@ -42,6 +48,10 @@ function addProductToCart(id, name, price) {
     createProductInCart(id);
 }
 
+/**
+ * Функция подсчитывает количество продуктов в корзине.
+ * @returns {number} - количество продуктов в корзине.
+ */
 function getCartTotalCount() {
 //   return Object.values(cart).reduce((acc, product) => acc + product.count, 0);
    const productsArr = Object.values(cart);
@@ -52,11 +62,19 @@ function getCartTotalCount() {
    return count;
 }
 
+/**
+ * Функция подсчитывает итоговую сумму, выбранных продуктов в корзине.
+ * @returns {number} - итоговая сумма, выбранных продуктов в корзине.
+ */
 function getCartTotalPrice() {
     return Object.values(cart)
     .reduce((acc, product) => acc + product.count * product.price, 0);
 }
 
+/**
+ * Функция создает новую информацию о выбранном продукте в корзине.
+ * @param {number} productId - Id продукта.
+ */
 function createNewProductInCart(productId) {
     const productRow = `
     <div class="cartBoxRow" data-id="${productId}">
@@ -73,6 +91,10 @@ function createNewProductInCart(productId) {
   cartTotalEl.insertAdjacentHTML("beforebegin", productRow);
 }
 
+/**
+ * Функция добавляет информацию о выбранном продукте в корзину.
+ * @param {number} productId - Id продукта.
+ */
 function createProductInCart(productId) {
     const cartBoxRowEl = cartBoxEl
     .querySelector(`.cartBoxRow[data-id="${productId}"]`);
